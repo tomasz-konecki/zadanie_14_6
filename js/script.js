31c0d092025ac4ba7ae17067b87daac035a03c3e
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-09-11 20:22:11 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onClick: this.increment},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
